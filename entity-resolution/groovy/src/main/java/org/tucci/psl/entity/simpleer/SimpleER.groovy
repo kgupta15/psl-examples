4e72ddb8fe1926fe93bd3e915fff745515e05c25
daemonslayer <kpgupta98@gmail.com>
2017-10-07 15:50:34 +0530

    src init

---
+++ entity-resolution/groovy/src/main/java/org/tucci/psl/entity/simpleer/SimpleER.groovy
@@ -0,0 +1,44 @@
+package org.tucci.psl.entity.simpleer;
+
+import org.linqs.psl.application.inference.MPEInference;
+import org.linqs.psl.config.ConfigBundle;
+import org.linqs.psl.config.ConfigManager;
+import org.linqs.psl.database.Database;
+import org.linqs.psl.database.DatabasePopulator;
+import org.linqs.psl.database.DataStore;
+import org.linqs.psl.database.Partition;
+import org.linqs.psl.database.Queries;
+import org.linqs.psl.database.ReadOnlyDatabase;
+import org.linqs.psl.database.loading.Inserter;
+import org.linqs.psl.database.rdbms.driver.H2DatabaseDriver;
+import org.linqs.psl.database.rdbms.driver.H2DatabaseDriver.Type;
+import org.linqs.psl.database.rdbms.RDBMSDataStore;
+import org.linqs.psl.groovy.PSLModel;
+import org.linqs.psl.model.atom.Atom;
+import org.linqs.psl.model.predicate.StandardPredicate;
+import org.linqs.psl.model.term.ConstantType;
+import org.linqs.psl.utils.dataloading.InserterUtils;
+import org.linqs.psl.utils.evaluation.printing.AtomPrintStream;
+import org.linqs.psl.utils.evaluation.printing.DefaultAtomPrintStream;
+import org.linqs.psl.utils.evaluation.statistics.ContinuousPredictionComparator;
+import org.linqs.psl.utils.evaluation.statistics.DiscretePredictionComparator;
+import org.linqs.psl.utils.evaluation.statistics.DiscretePredictionStatistics;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import groovy.time.TimeCategory;
+import java.nio.file.Paths;
+
+
+public class SimpleER {
+
+    /**
+     * Runs the PSL program from the command line with specified arguments
+     * @param args - Arguments for program options
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
